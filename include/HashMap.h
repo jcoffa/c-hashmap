dbde371e7582698d26b7139a77394ac033bdce35
Joseph Coffa <josephco440@gmail.com>
2020-07-29 20:57:57 -0400

    Initial commit
    The very basic functions have been implemented already, namely
    hashmapNew, hashmapNewBuckets, hashentryNew, hashmapClear, hashmapFree,
    hashmapInsert (some functionality missing; explained in TODO),
    hashmapGet, hashmapLength, hashmapToString, and hashmapPrint.

---
+++ include/HashMap.h
@@ -0,0 +1,268 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/***********************
+ * PREPROCESSOR MACROS *
+ ***********************/
+
+/*
+ * The highest allowed ratio of (number of entries in hash map):(total buckets available)
+ * before the hash map needs to be resized and the indices recalculated.
+ *
+ * With this current value, the hash map will be resized after two-thirds of its buckets are filled.
+ */
+#define LOAD_FACTOR (2.0 / 3.0)
+
+// Default number of buckets in a new hash map
+#define DEFAULT_BUCKETS (16)
+
+
+/*
+ * A hash map with this amount of buckets is considered "large" and grows at a reduced rate
+ * compared to a "small" hash map.
+ *
+ * A "small" hash map is resized to contain 4 times as many buckets as it did previously,
+ * while a "large" hash map is resized to only contain 2 times as many buckets as it did previously.
+ */
+#define HASHMAP_LARGE_SIZE (65536)	// = 2^16
+#define HASHMAP_IS_LARGE(map) (map->num_buckets >= HASHMAP_LARGE_SIZE)
+
+
+/*
+ * Signals to the `hashmapNew` and `hashmapNewBuckets` functions to use the default hash function
+ * of djb2 for string (char *) data instead of requiring the caller to provide their own hash function.
+ */
+#define DEFAULT_HASH NULL
+
+
+/**************
+ * STRUCTURES *
+ **************/
+
+/*
+ * A key-value pair to be stored by the hash map.
+ *
+ * The key's full hash value is also stored so that it does not have to
+ * be recalculated during lookup or whenever the hash map is resized.
+ */
+typedef struct hashMapEntry {
+	int64_t hash;
+	void *key;
+	void *value;
+} HashEntry;
+
+/*
+ * Metadata head of the hash map. 
+ * Contains the function pointers for working with the abstracted hash map data.
+ *
+ * =======================================================================================
+ *
+ * My other attempt at writing a hash map took inspiration from CPython3.6 dict's,
+ * but it proved too complicated for my tiny ape brain. This is a much simpler
+ * and naive (but still quite effective) approach to implementing a hash map in C.
+ *
+ * The main difference between this implementation and the one used by Python
+ * is memory usage; the Python version uses much less memory thanks to its
+ * clever design choice of using an index array as the hash table which maps
+ * to indices in a compact sequential array. This means the actual "hash table"
+ * contains numbers that are only a couple bytes large instead of the comparatively
+ * massive 24 byte HashEntry structure used by this implementation.
+ *
+ * Resizing the hash map is also slower than CPython's in this implementation,
+ * and there may be more collisions due to the use of linear probing instead
+ * of the more complex linear congruential generator sequence (5*i + perturb + 1)
+ *
+ * =======================================================================================
+ *
+ * This hash map uses the "open addressing" approach to collision handling, and uses a
+ * naive "linear probing" method to handle collisions. This means that this hash map
+ * implementation is decently dependant on having a good hash function to spread out
+ * its elements. Here's a pretty incredible Stack Exchange post on how some popular
+ * hash functions perform with English words, numbers, and GUID's as their data:
+ * 		https://softwareengineering.stackexchange.com/a/145633
+ *
+ * This implementation will resize and rehash its entries once its load factor exceeds 2/3;
+ * quadrupling the size of the hash map each resize (until its number of buckets reaches
+ * HASHMAP_LARGE_SIZE, at which point it is doubled each resize)
+ */
+typedef struct hashMapHead {
+	HashEntry **entries;	// The key-value pairs stored in the hash map
+	long length;		// The number of entries currently in the hash map
+	long num_buckets;	// The maximum number of entries the hash map can hold.
+	                 	// For a new hash map, this field is equal to DEFAULT_BUCKETS.
+
+	int64_t (*hash)(void *);		// The hash function to turn a key into an 8-byte signed integer
+	void (*deleteValue)(void *);	// Function pointer to free a value in the hash map
+	char *(*printValue)(void *);	// Function pointer to create a string from a value in the hash map
+	void (*deleteKey)(void *);		// Function pointer to free a key in the hash map
+	char *(*printKey)(void *);		// Function pointer to create a string from a key in the hash map
+} HashMap;
+
+
+/*************
+ * FUNCTIONS *
+ *************/
+
+/*
+ * Function to initialize the HashMap metadata head to the appropriate function pointers.
+ * Allocates memory to the struct, unless any of the function pointers except `hash` are NULL.
+ * If the `hash` function pointer is NULL, the djb2 hashing algorithm for strings (char *) is used.
+ * Passing the literal NULL into a function isn't very descriptive and looks odd, so callers are
+ * encouraged to use the DEFAULT_HASH macro instead.
+ *
+ * If any other function pointers are NULL, then NULL is returned instead and no memory is allocated.
+ * NULL is also returned if any memory allocation fails.
+ *
+ * The HashMap provides an interface to a generic collection of keys each mapped to a generic value.
+ * The first function pointer is the hash function used to turn keys into 8-byte signed integer hashes
+ * in typical hash map fashion. The four remaining function pointers allow the struct to print and delete
+ * its keys and values.
+ * The (void *) arguments are to be casted into their proper data type (i.e. whatever
+ * data type the hash map's keys and values will be) and do as follows:
+ *
+ * 	void deleteVal(void *toDelete) : free all memory associated with the value `toDelete`
+ * 	char *printVal(void *toPrint)  : return a string representation of the value `toPrint`
+ * 	void deleteKey(void *toDelete) : free all memory associated with the key `toDelete`
+ * 	char *printKey(void *toPrint)  : return a string representation of the key `toPrint`
+ *
+ * Examples of these functions are provided for string (char *) data in the README.
+ */
+HashMap *hashmapNew(int64_t (*hash)(void *), void (*deleteValue)(void *), char *(*printValue)(void *), \
+        void (*deleteKey)(void *), char *(*printKey)(void *));
+
+
+/*
+ * Similar to the `hashmapNew` function, except the number of starting buckets can be given manually.
+ * Useful for when you are guaranteed to need a large hash map and the default value will cause
+ * a resize or two that could be avoided with the use of this function.
+ *
+ * The starting number of buckets, `B`, will be converted to the smallest power of two, `N`,
+ * such that `B <= N`. For example, if 20 is given as the starting number of buckets, the created
+ * hash map will actually have 32 buckets instead.
+ */
+HashMap *hashmapNewBuckets(long num_buckets, int64_t (*hash)(void *), void (*deleteValue)(void *), \
+        char *(*printValue)(void *), void (*deleteKey)(void *), char *(*printKey)(void *));
+
+
+/*
+ * Frees every key and value in the hash map without deleting the hash map itself.
+ * The hash map is NOT resized after removing all its entries. This could potentially
+ * waste lots of memory if the hash map was decently large before it was cleared.
+ */
+void hashmapClear(HashMap *map);
+
+
+/*
+ * Frees all memory associated with the hash map, including the hash map itself.
+ */
+void hashmapFree(HashMap *map);
+
+
+/*
+ * Sets the value associated with the given key within the hash map,
+ * replacing (and freeing) the data that was previously associated with that key if necessary.
+ *
+ * Returns false if the hash map is NULL, if any memory allocation fails, or if the key is NULL.
+ * Hash map values are allowed to be NULL, but keys are not.
+ *
+ * Returns true otherwise, indicating a successful operation.
+ */
+bool hashmapInsert(HashMap *map, void *key, void *value);
+
+
+/*
+ * Returns the value associated with the given key.
+ *
+ * Returns NULL if the hash map itself is NULL, or if the key is not present in the hash map.
+ */
+void *hashmapGet(const HashMap *map, void *key);
+
+
+/*
+ * Returns and removes the value associated with the given key.
+ * The key is also removed from the hash map.
+ *
+ * Returns NULL if the hashmap itself is NULL, or if the key is not present in the hash map.
+ */
+void *hashmapRemove(HashMap *map, void *key);
+
+
+/*
+ * Removes the value associated with the given key and the key itself from the hash map.
+ * Both the key and value are freed after removal.
+ *
+ * Returns false if the hashmap is NULL, or if the key is not present in the hash map.
+ *
+ * Returns true otherwise, indicating a successful deletion.
+ *
+ * NOTE: it's called `hashmapDeleteKey` and not just `hashmapDelete` because I thought that sounded
+ * too much like the `hashmapClear` and `hashmapFree` functions and might be confusing.
+ */
+bool hashmapDeleteKey(HashMap *map, void *key);
+
+
+/*
+ * Returns true if the given key is mapped to a value inside the hash map, and false otherwise.
+ *
+ * False is also returned when the hash map is a NULL pointer.
+ */
+bool hashmapContains(const HashMap *map, void *key);
+
+
+/*
+ * Returns the number of key-value pair entries currently stored in the hash map.
+ *
+ * If the hash map is NULL, -1 is returned.
+ */
+long hashmapLength(const HashMap *map);
+
+
+/*
+ * Returns true if the hash map contains no elements, and false otherwise.
+ *
+ * False is also returned when the hash map is a NULL pointer.
+ */
+bool hashmapIsEmpty(const HashMap *map);
+
+
+/*
+ * Returns a string representing the value associated with the given key,
+ * using the hash map's `printVal` function pointer to create the string.
+ *
+ * The string must be freed by the calling function after use.
+ */
+char *hashmapValueToString(const HashMap *map, void *value);
+
+
+/*
+ * A convenient alias for printing the string returned by `hashmapValuetoString(map, key)`
+ * and then freeing the string that was created after printing it.
+ * A newline is printed after the string is done printing.
+ */
+void hashmapPrintValue(const HashMap *map, void *value);
+
+
+/*
+ * Returns a string representing the entire hash map using it's `printKey` and `printVal`
+ * function pointers to create the string.
+ *
+ * The string must be freed by the calling function after use.
+ */
+char *hashmapToString(const HashMap *map);
+
+
+/*
+ * A convenient alias for printing the string returned by `hashmapToString(map)`
+ * and then freeing the string that was created after printing it.
+ * A newline is printed after the string is done printing.
+ */
+void hashmapPrint(const HashMap *map);
+
+#endif	// HASHMAP_H
+
---
+++ src/HashMap.c
@@ -0,0 +1,344 @@
+#include "HashMap.h"
+
+
+static int64_t djb2x(void *key) {
+	char *str = (char *)key;
+	int64_t hash = 5381;
+	int c;
+
+	while ((c = *str++)) {
+		hash = ((hash << 5) + hash) ^ c;	// (hash * 33) XOR c
+	}
+
+	return hash;
+}
+
+/*
+ * Returns the smallest power of 2, N, such that: `x <= N`.
+ *
+ * For example, closestPow2(20) = 32 (32 is the smallest power of 2 that is >= 20)
+ */
+static int closestPow2(long x) {
+	int toReturn = 1;
+
+	while (toReturn < x) {
+		toReturn *= 2;
+	}
+
+	return toReturn;
+}
+
+
+HashMap *hashmapNew(int64_t (*hash)(void *), void (*deleteVal)(void *), char *(*printVal)(void *), \
+        void (*deleteKey)(void *), char *(*printKey)(void *)) {
+
+	return hashmapNewBuckets(DEFAULT_BUCKETS, hash, deleteVal, printVal, deleteKey, printKey);
+}
+
+
+HashMap *hashmapNewBuckets(long num_buckets, int64_t (*hash)(void *), void (*deleteValue)(void *), \
+        char *(*printValue)(void *), void (*deleteKey)(void *), char *(*printKey)(void *)) {
+
+	if (deleteValue == NULL || printValue == NULL || deleteKey == NULL || printKey == NULL) {
+		return NULL;
+	}
+
+	HashMap *toReturn = malloc(sizeof(HashMap));
+
+	if (toReturn == NULL) {
+		return NULL;
+	}
+
+	// Have to use calloc to set all memory to 0
+	toReturn->entries = calloc(closestPow2(num_buckets), sizeof(HashEntry));
+	if (toReturn->entries == NULL) {
+		free(toReturn);
+		return NULL;
+	}
+
+	// If hash function was not given, then use the djb2x hash function for string (char *) data
+	if (hash == DEFAULT_HASH) {
+		toReturn->hash = djb2x;
+	} else {
+		toReturn->hash = hash;
+	}
+
+	toReturn->length = 0;
+	toReturn->num_buckets = num_buckets;
+	toReturn->deleteValue = deleteValue;
+	toReturn->printValue = printValue;
+	toReturn->deleteKey = deleteKey;
+	toReturn->printKey = printKey;
+
+	return toReturn;
+}
+
+
+/*
+ * Allocates and returns a new HashEntry to be put into a HashMap.
+ *
+ * Returns NULL instead if any memory allocation fails, or the key is NULL.
+ * Hash map values are allowed to be NULL, but keys are not.
+ */
+static HashEntry *hashentryNew(int64_t hash, void *key, void *value) {
+	if (key == NULL) {
+		return NULL;
+	}
+
+	HashEntry *toReturn = malloc(sizeof(HashEntry));
+
+	if (toReturn == NULL) {
+		return NULL;
+	}
+
+	toReturn->hash = hash;
+	toReturn->key = key;
+	toReturn->value = value;
+
+	return toReturn;
+}
+
+
+void hashmapClear(HashMap *map) {
+	if (map == NULL) {
+		return;
+	}
+
+	// This while loop's condition may seem odd, but this is an attempt to avoid useless iterations.
+	// Since hash maps are full of empty space, it's not unreasonable for there to be a pretty
+	// decent amount of empty buckets at the end of the hash map's entries array.
+	//
+	// This while loop condition basically says "while there are entries we haven't freed yet".
+	HashEntry *entry;
+	long i = 0;
+	while (map->length > 0 && i < map->num_buckets) {
+		entry = (map->entries)[i];
+
+		if (entry != NULL) {
+			map->deleteValue(entry->value);
+			map->deleteKey(entry->key);
+			free(entry);
+
+			(map->entries)[i] = NULL;
+			(map->length)--;
+		}
+
+		i++;
+	}
+}
+
+
+void hashmapFree(HashMap *map) {
+	if (map == NULL) {
+		return;
+	}
+
+	hashmapClear(map);
+	free(map->entries);
+	free(map);
+}
+
+
+/*
+ * The hash map can't be NULL before calling this function.
+ *
+ * Returns true if the hash map's load factor has exceeded the allowable amount (i.e. LOAD_FACTOR)
+ * and needs to be resized.
+ *
+ * Returns false otherwise.
+ *
+ * There is a special case for particularly small hash maps which can only be created by
+ * using the `hashmapNewBuckets` function with very small amounts of buckets. Recall that
+ * there must be at least 1 empty space in the hash map or else the lookup will never terminate.
+ *
+ * For example, imagine that a hash map has exactly 2 buckets and only 1 bucket is filled.
+ * The load factor (of 0.5) is within the allowable amount (of 0.67) so the hash map will not be resized,
+ * but adding another element would completely fill the hash map. This case must also signal
+ * that a resize is necessary, or else future lookups will infinite loop.
+ *
+ * This special case where an insertion would compromise the integrity of hash map lookups
+ * is what the second condition represents.
+ */
+static inline bool hashmapNeedsResize(const HashMap *map) {
+	if (((double)(map->length) / (double)(map->num_buckets)) > LOAD_FACTOR) {
+		return true;
+	}
+	return map->length == map->num_buckets-1;
+}
+
+
+/*
+ * Resizes the hash map so that it holds four times as many buckets as it did previously.
+ * Once the hash map becomes sufficiently large (its number of buckets is at least HASHMAP_LARGE_SIZE)
+ * the hash map is only resized to hold twice as many buckets as it did previously.
+ *
+ * All its entries are reinserted into the buckets after resizing, potentially receiving
+ * a new position in the hash map thanks to the increase in open space and size.
+ *
+ * This is a pretty expensive operation that should performed as infrequently as possible.
+ * This cost is part of the reason why these hash maps are resized to be so massive.
+ *
+ * Returns false if the hash map is NULL, or if any memory allocation fails.
+ * Returns true otherwise, indicating a successful operation.
+ */
+static bool hashmapResize(HashMap *map) {
+	printf("hashmapResize is unimplemented");
+	return false;
+}
+
+
+bool hashmapInsert(HashMap *map, void *key, void *value) {
+	if (map == NULL || key == NULL) {
+		return false;
+	}
+
+	// Resize the hashmap
+	if (hashmapNeedsResize(map)) {
+		// hashmapResize returns false on an error
+		if (!hashmapResize(map)) {
+			// If the resize fails, then there's no sense in trying to continue
+			return false;
+		}
+	}
+
+	int64_t hashVal = map->hash(key);
+	HashEntry *toInsert = hashentryNew(hashVal, key, value);
+	if (toInsert == NULL) {
+		return false;
+	}
+
+	long i = labs(hashVal) % (map->num_buckets);
+
+	// Find an empty spot in the hash map by linear probing
+	while ((map->entries)[i] != NULL) {
+		// TODO check if current entry has the same hash as what we're trying to insert.
+		// If it does, then free the old value (dont have to free the entire entry) and replace it.
+		// Make sure to not increment the hash map's length.
+
+		// Wrap the index around the end of the array if necessary
+		i = (i+1) % (map->num_buckets);	
+	}
+
+	(map->entries)[i] = toInsert;
+	(map->length)++;
+
+	return true;
+}
+
+
+void *hashmapGet(const HashMap *map, void *key) {
+	if (map == NULL || key == NULL) {
+		return NULL;
+	}
+
+	void *toReturn = NULL;
+	int64_t hashVal = map->hash(key);
+	long i = labs(hashVal) % (map->num_buckets);
+	HashEntry *cur = (map->entries)[i];
+
+	while (cur != NULL) {
+		if (cur->hash == hashVal) {
+			toReturn = cur->value;
+			break;
+		}
+
+		// Wrap the index around the end of the array if necessary
+		i = (i+1) % (map->num_buckets);
+		cur = (map->entries)[i];
+	}
+
+	return toReturn;
+}
+
+
+void *hashmapRemove(HashMap *map, void *key) {
+	printf("hashmapRemove is unimplemented\n");
+	return NULL;
+}
+
+
+bool hashmapDeleteKey(HashMap *map, void *key) {
+	printf("hashmapDeleteKey is unimplemented\n");
+	return false;
+}
+
+
+bool hashmapContains(const HashMap *map, void *key) {
+	printf("hashmapContains is unimplemented\n");
+	return false;
+}
+
+
+long hashmapLength(const HashMap *map) {
+	if (map == NULL) {
+		return -1;
+	}
+	return map->length;
+}
+
+
+bool hashmapIsEmpty(const HashMap *map) {
+	printf("hashmapIsEmpty is unimplemented\n");
+	return false;
+}
+
+
+char *hashmapValueToString(const HashMap *map, void *value) {
+	printf("hashmapValueToString is unimplemented\n");
+	return NULL;
+}
+
+
+void hashmapPrintValue(const HashMap *map, void *value) {
+	printf("hashmapPrintValue is unimplemented\n");
+}
+
+
+char *hashmapToString(const HashMap *map) {
+	size_t length = 2;	// starting length of 2 for '{' and '}'
+	char *toReturn = toReturn = malloc(length+1);	// +1 for null terminator
+	char *keyStr;
+	char *valueStr;
+	strcpy(toReturn, "{");
+
+	if (map == NULL || map->length == 0) {
+		return strcat(toReturn, "}");
+	}
+
+	HashEntry *entry;
+	for (long i = 0; i < map->num_buckets; i++) {
+		entry = (map->entries)[i];
+		if (entry == NULL) {
+			continue;
+		}
+
+		keyStr = map->printKey(entry->key);
+		valueStr = map->printValue(entry->value);
+
+		// +4 for a ": " to separate key and value and ", " to separate entries
+		length += strlen(keyStr) + strlen(valueStr) + 4;
+
+		toReturn = realloc(toReturn, length+1);	// +1 for null terminator
+		strcat(toReturn, keyStr);
+		strcat(toReturn, ": ");
+		strcat(toReturn, valueStr);
+		strcat(toReturn, ", ");
+
+		free(keyStr);
+		free(valueStr);
+	}
+
+	// Replace the trailing ", " with just "}"
+	toReturn[length-2] = '\0';
+	toReturn[length-3] = '}';
+
+	return toReturn;
+}
+
+
+void hashmapPrint(const HashMap *map) {
+	char *toPrint = hashmapToString(map);
+	printf("%s\n", toPrint);
+	free(toPrint);
+}
+
